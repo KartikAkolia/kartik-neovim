5b013522da2071815f67eb93c2fec5e38f5355ce
KartikAkolia <kartikakolia@outlook.com>
2025-08-08 14:33:46 +0100

    Update which-key.lua
    Update to the new which-key spec format

--- lua/k/plugins/which-key.lua
+++ lua/k/plugins/which-key.lua
@@ -3,19 +3,18 @@ return {
   event = "VeryLazy",
   opts = {
     plugins = { spelling = true },
+    spec = {
+      {
+        mode = { "n", "v" },
+        { "<leader>f", group = "find" },
+        { "<leader>t", group = "tree" },
+        { "<leader>s", group = "search" },
+        { "<leader>l", group = "lsp" },
+        { "<leader>x", group = "diagnostics" },
+      },
+    },
   },
   config = function(_, opts)
-    local wk = require("which-key")
-    wk.setup(opts)
-    wk.register({
-      mode = { "n", "v" },
-      { "<leader>f", group = "find" },
-      { "<leader>l", group = "lsp" },
-      { "<leader>s", group = "search" },
-      { "<leader>t", group = "tree" },
-      { "<leader>x", group = "diagnostics" },
-      -- `<leader>h` is a standalone mapping defined in keymaps.lua,
-      -- so it must NOT appear here to avoid duplicates or warnings.
-    })
+    require("which-key").setup(opts)
   end,
 }
